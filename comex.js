3b4ab6ca2e8d6266cbb9c09f0f341a0b5bf05884
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-09 14:11:44 +0800

    Improved code.

--- comex.js
+++ comex.js
@@ -34,6 +34,9 @@
 			"file": "comex.js",
 			"module": "comex",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/comex.git",
 			"test": "comex-test.js",
